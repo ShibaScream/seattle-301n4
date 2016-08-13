6f8f55e7e480ed1163c74a03b2ebf4250014ff65
Frank Iriarte <jurrish@gmail.com>
2016-08-13 11:04:37 -0700

    working on searchbar. done with populating state/city

--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -2,6 +2,44 @@
 
   // TODO: Write the code to populate your filters, and enable the search queries here in search.js
   // TODO: You will also interact with the map.js file here
+  var searchView = {};
+  searchView.populateFilters = function () {
+    webDB.execute('SELECT state FROM zips;', function(rows) {
+      searchView.loadAll('state-select', 'state', rows);
+      // ADD event listener to state
+      $('#state-select').on('change', function() {
+        console.log($(this).val());
+        if ($(this).val().length > 0) {
+          webDB.execute(
+            [
+              {
+                'sql': 'SELECT city FROM zips WHERE state = ?;',
+                'data': [$(this).val()]//may have to change this value
+              }
+            ],
+            function (rows) {
+              searchView.loadAll('city-select', 'city', rows);
+            }
+          );
+        }
+      });
+        // EVENT LISTENER calls webDB.execute to select cities from zip where state equals return value from event listener
+          // run loadAll to add cities to filter
+    });
+  };
 
-
-})(window)
+  searchView.loadAll = function(id, option, rows) {
+    var $filter = $('#' + id);
+    var results = rows.map(function (row) {
+      return row[option];
+    }).filter(function (result, i, arr) {
+      return arr.indexOf(result) === i;
+    });
+    results.sort();
+    results.map(function (result) {
+      var optiontag = '<option value="' + result + '">' + result + '</option>';
+      $filter.append(optiontag);
+    });
+  };
+  searchView.populateFilters();
+})(window);
