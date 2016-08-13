13da2204001ccb909de451b63b3d841cba5f8e7b
Dan Peters <daniel.r.peters@icloud.com>
2016-08-13 14:39:34 -0700

    getting closer, but having issues with remove city options when changing states.

--- class-09-joins-and-relations/lab/zip-codes/scripts/search.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/search.js
@@ -3,24 +3,29 @@
   // TODO: Write the code to populate your filters, and enable the search queries here in search.js
   // TODO: You will also interact with the map.js file here
   var searchView = {};
+  
   searchView.populateFilters = function () {
     webDB.execute('SELECT state FROM zips;', function(rows) {
       searchView.loadAll('state-select', 'state', rows);
       // ADD event listener to state
       $('#state-select').on('change', function() {
-        console.log($(this).val());
-        if ($(this).val().length > 0) {
+        $('#city-select').val('');
+        $('#city-select').slice(1).remove();
+        var $state = $(this).val();
+        if ($state.length > 0) {
           webDB.execute(
             [
               {
                 'sql': 'SELECT city FROM zips WHERE state = ?;',
-                'data': [$(this).val()]//may have to change this value
+                'data': [$state]
               }
             ],
             function (rows) {
               searchView.loadAll('city-select', 'city', rows);
               $('#city-select').on('change', function() {
+                var $city = $(this).val();
                 //add marker drop
+                searchView.searchAndAddMarker(['city', 'state'], [$city, $state]);
               });
             }
           );
@@ -48,25 +53,37 @@
   $('#search').on('submit', function(event) {
     event.preventDefault();
     var $result = $('input[name="zip"]').val();
-    console.log($result);
-//add integer validation
+    //add integer validation
+    searchView.searchAndAddMarker(['zip'], [$result]);
+  });
+
+  searchView.searchAndAddMarker = function (joinsArr, filtersArr) {
+    var parameters = joinsArr.reduce(function(prev, current, index) {
+      current += ' = ?';
+      if (index > 0) {
+        current = ' AND ' + current;
+      }
+      return prev + current
+    }, '');
+    
+    console.log(parameters);
+    
     webDB.execute(
       [
         {
-          'sql':'SELECT * FROM zips WHERE zip = ?;',
-          'data': [$result]
+          'sql':'SELECT * FROM zips WHERE ' + parameters,
+          'data': filtersArr
         }
       ],
       function(rows) {
+        // DEBUG
         console.log(rows);
         //marker drop
         rows.forEach(function(row) {
           createMarker(row);
         });
-      }
-    );
-  });
-
-
+    });
+  };
+  
   searchView.populateFilters();
 })(window);
