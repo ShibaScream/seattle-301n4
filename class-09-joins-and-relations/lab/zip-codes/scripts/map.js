07db81eb09448def44eaad139a038e764c2238b4
Dan Peters <daniel.r.peters@icloud.com>
2016-08-13 09:52:23 -0700

    fix indentation

--- class-09-joins-and-relations/lab/zip-codes/scripts/map.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/map.js
@@ -4,36 +4,36 @@ function initMap() {
   
   var styles = [
 
-      // hide Google's labels
-      {
-          featureType: "all",
-          elementType: "labels",
-          stylers: [
-              {visibility: "off"}
-          ]
-      },
+    // hide Google's labels
+    {
+      featureType: "all",
+      elementType: "labels",
+      stylers: [
+          {visibility: "off"}
+      ]
+    },
 
-      // hide roads
-      {
-          featureType: "road",
-          elementType: "geometry",
-          stylers: [
-              {visibility: "off"}
-          ]
-      }
+    // hide roads
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+          {visibility: "off"}
+      ]
+    }
 
   ];
   
   var options = {
-      center: {lat: 47.611435, lng: -122.330456},
-      disableDefaultUI: true,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      maxZoom: 14,
-      panControl: true,
-      styles: styles,
-      zoom: 8,
-      zoomControl: true,
-      scrollwheel: true
+    center: {lat: 47.611435, lng: -122.330456},
+    disableDefaultUI: true,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    maxZoom: 14,
+    panControl: true,
+    styles: styles,
+    zoom: 8,
+    zoomControl: true,
+    scrollwheel: true
   };
   
   // Create a map object and specify the DOM element for display.
