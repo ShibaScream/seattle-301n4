fa4403b7211e413d8569c0d5f0f73ffe0317a3a5
Dan Peters <daniel.r.peters@icloud.com>
2016-08-13 09:39:16 -0700

    added some initial styling to the map UI

--- class-09-joins-and-relations/lab/zip-codes/scripts/map.js
+++ class-09-joins-and-relations/lab/zip-codes/scripts/map.js
@@ -1,10 +1,41 @@
 function initMap() {
+  
+  var styles = [
+
+      // hide Google's labels
+      {
+          featureType: "all",
+          elementType: "labels",
+          stylers: [
+              {visibility: "off"}
+          ]
+      },
+
+      // hide roads
+      {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [
+              {visibility: "off"}
+          ]
+      }
+
+  ];
+  
+  var options = {
+      center: {lat: 47.611435, lng: -122.330456},
+      disableDefaultUI: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      maxZoom: 14,
+      panControl: true,
+      styles: styles,
+      zoom: 8,
+      zoomControl: true,
+      scrollwheel: true
+  };
+  
   // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 47.611435, lng: -122.330456},
-    scrollwheel: true,
-    zoom: 8
-  });
+  var map = new google.maps.Map(document.getElementById('map'), options);
 
   // TODO: Follow the Google Maps API docs to create markers on the map based on the search options on the home page.
 }
